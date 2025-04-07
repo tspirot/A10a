1c30d1ea776b7163e2671082ab4f5eed7b9dbbcb
Boban Blagojević <boban.blagojevic@tsp.edu.rs>
2025-04-07 13:01:10 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace A10a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonIzmena = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonAnaliza = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonOAplikaciji = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonIzlaz = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSifra = new System.Windows.Forms.TextBox();
+            this.textBoxIme = new System.Windows.Forms.TextBox();
+            this.textBoxPrezime = new System.Windows.Forms.TextBox();
+            this.textBoxAdresa = new System.Windows.Forms.TextBox();
+            this.comboBoxGrad = new System.Windows.Forms.ComboBox();
+            this.textBoxTelefon = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(50, 50);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonIzmena,
+            this.toolStripButtonAnaliza,
+            this.toolStripButtonOAplikaciji,
+            this.toolStripButtonIzlaz});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1274, 77);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonIzmena
+            // 
+            this.toolStripButtonIzmena.Image = global::A10a.Properties.Resources.update;
+            this.toolStripButtonIzmena.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonIzmena.Name = "toolStripButtonIzmena";
+            this.toolStripButtonIzmena.Size = new System.Drawing.Size(61, 74);
+            this.toolStripButtonIzmena.Text = "Izmena";
+            this.toolStripButtonIzmena.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButtonAnaliza
+            // 
+            this.toolStripButtonAnaliza.Image = global::A10a.Properties.Resources.charts;
+            this.toolStripButtonAnaliza.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonAnaliza.Name = "toolStripButtonAnaliza";
+            this.toolStripButtonAnaliza.Size = new System.Drawing.Size(62, 74);
+            this.toolStripButtonAnaliza.Text = "Analiza";
+            this.toolStripButtonAnaliza.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButtonOAplikaciji
+            // 
+            this.toolStripButtonOAplikaciji.Image = global::A10a.Properties.Resources.info_icon;
+            this.toolStripButtonOAplikaciji.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonOAplikaciji.Name = "toolStripButtonOAplikaciji";
+            this.toolStripButtonOAplikaciji.Size = new System.Drawing.Size(87, 74);
+            this.toolStripButtonOAplikaciji.Text = "O aplikaciji";
+            this.toolStripButtonOAplikaciji.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButtonIzlaz
+            // 
+            this.toolStripButtonIzlaz.Image = global::A10a.Properties.Resources.exit;
+            this.toolStripButtonIzlaz.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonIzlaz.Name = "toolStripButtonIzlaz";
+            this.toolStripButtonIzlaz.Size = new System.Drawing.Size(54, 74);
+            this.toolStripButtonIzlaz.Text = "Izlaz";
+            this.toolStripButtonIzlaz.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Šifra";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Prezime";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Adresa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Grad";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 242);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Telefon";
+            // 
+            // textBoxSifra
+            // 
+            this.textBoxSifra.Location = new System.Drawing.Point(94, 89);
+            this.textBoxSifra.Name = "textBoxSifra";
+            this.textBoxSifra.ReadOnly = true;
+            this.textBoxSifra.Size = new System.Drawing.Size(138, 22);
+            this.textBoxSifra.TabIndex = 7;
+            // 
+            // textBoxIme
+            // 
+            this.textBoxIme.Location = new System.Drawing.Point(94, 118);
+            this.textBoxIme.Name = "textBoxIme";
+            this.textBoxIme.Size = new System.Drawing.Size(255, 22);
+            this.textBoxIme.TabIndex = 8;
+            // 
+            // textBoxPrezime
+            // 
+            this.textBoxPrezime.Location = new System.Drawing.Point(94, 146);
+            this.textBoxPrezime.Name = "textBoxPrezime";
+            this.textBoxPrezime.Size = new System.Drawing.Size(255, 22);
+            this.textBoxPrezime.TabIndex = 9;
+            // 
+            // textBoxAdresa
+            // 
+            this.textBoxAdresa.Location = new System.Drawing.Point(94, 176);
+            this.textBoxAdresa.Name = "textBoxAdresa";
+            this.textBoxAdresa.Size = new System.Drawing.Size(339, 22);
+            this.textBoxAdresa.TabIndex = 10;
+            // 
+            // comboBoxGrad
+            // 
+            this.comboBoxGrad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxGrad.FormattingEnabled = true;
+            this.comboBoxGrad.Location = new System.Drawing.Point(94, 205);
+            this.comboBoxGrad.Name = "comboBoxGrad";
+            this.comboBoxGrad.Size = new System.Drawing.Size(255, 24);
+            this.comboBoxGrad.TabIndex = 11;
+            // 
+            // textBoxTelefon
+            // 
+            this.textBoxTelefon.Location = new System.Drawing.Point(94, 236);
+            this.textBoxTelefon.Name = "textBoxTelefon";
+            this.textBoxTelefon.Size = new System.Drawing.Size(192, 22);
+            this.textBoxTelefon.TabIndex = 12;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Courier New", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(452, 102);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(770, 388);
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1023, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Telefon";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(866, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 16);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Grad";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(765, 83);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 16);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Adresa";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(666, 83);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 16);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Prezime";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(554, 83);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 16);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Ime";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(472, 83);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(34, 16);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Šifra";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1274, 500);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBoxTelefon);
+            this.Controls.Add(this.comboBoxGrad);
+            this.Controls.Add(this.textBoxAdresa);
+            this.Controls.Add(this.textBoxPrezime);
+            this.Controls.Add(this.textBoxIme);
+            this.Controls.Add(this.textBoxSifra);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Pecaroši";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonIzmena;
+        private System.Windows.Forms.ToolStripButton toolStripButtonAnaliza;
+        private System.Windows.Forms.ToolStripButton toolStripButtonOAplikaciji;
+        private System.Windows.Forms.ToolStripButton toolStripButtonIzlaz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxSifra;
+        private System.Windows.Forms.TextBox textBoxIme;
+        private System.Windows.Forms.TextBox textBoxPrezime;
+        private System.Windows.Forms.TextBox textBoxAdresa;
+        private System.Windows.Forms.ComboBox comboBoxGrad;
+        private System.Windows.Forms.TextBox textBoxTelefon;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A10a
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection konekcija = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\A10.mdf;Integrated Security=True");
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopuniComboGradova();
+            OsveziListuPecarosa();
+
+        }
+        private void PopuniComboGradova()
+        {
+            DataTable dtg = new DataTable();
+            string upit = "SELECT g.GradID AS Sifra, g.Grad AS Naziv " +
+                "FROM Grad AS g";
+            SqlDataAdapter dag = new SqlDataAdapter(upit,konekcija);
+            try
+            {
+                dag.Fill(dtg);
+                comboBoxGrad.DataSource = dtg;
+                comboBoxGrad.ValueMember = "Sifra";
+                comboBoxGrad.DisplayMember = "Naziv";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška " + ex.Message);
+            }
+            finally
+            {
+                konekcija.Close();
+            }
+        }
+        private void OsveziListuPecarosa()
+        {
+            string upit =
+                "SELECT p.PecarosID,p.Ime,p.Prezime,p.Adresa,g.Grad,p.Telefon " +
+                "FROM Pecaros AS p, Grad AS g " +
+                "WHERE p.GradID=g.GradID";
+            SqlDataAdapter adapter = new SqlDataAdapter(upit, konekcija);
+            try
+            {
+                dt.Clear();
+                listBox1.Items.Clear();
+                adapter.Fill(dt);
+                foreach (DataRow red in dt.Rows)
+                {
+                    string pec = String.Format(
+                        "{0,-3}{1,-15}{2,-15}{3,-20}{4,-15}{5,-15}", 
+                        red[0], red[1], red[2], red[3], red[4], red[5]
+                        );
+                    listBox1.Items.Add(pec);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška " + ex.Message);
+            }
+            finally
+            {
+                konekcija.Close();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sel= listBox1.SelectedIndex;
+            if (sel == -1) 
+                IsprazniKontrole();
+            else
+            {
+                DataRow red = dt.Rows[sel];
+                textBoxSifra.Text = red[0].ToString();
+                textBoxIme.Text = red[1].ToString();
+                textBoxPrezime.Text = red[2].ToString();
+                textBoxAdresa.Text = red[3].ToString();
+                comboBoxGrad.Text = red[4].ToString();
+                textBoxTelefon.Text = red[5].ToString();
+            }
+
+        }
+        private void IsprazniKontrole()
+        {
+            textBoxSifra.Text = "";
+            textBoxIme.Text = "";
+            textBoxPrezime.Text = "";
+            textBoxAdresa.Text = "";
+            comboBoxGrad.SelectedIndex = -1;
+            textBoxTelefon.Text = "";
+        }
+    }
+}
